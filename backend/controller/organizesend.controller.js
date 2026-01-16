46d25ad43118edf91f9365c316be76864dffae24
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-16 18:41:42 -0500

    working on the analysis part

--- backend/controller/organizesend.controller.js
+++ backend/controller/organizesend.controller.js
@@ -6,5 +6,33 @@ const client = new OpenAi({
 }); 
 
 const organizeAnalysis = async (req, res) => { 
-    // first i want to set up a try and catch case here 
-}
+    // first i want to set up a try and catch case here  
+    try {
+        // i need to get the query parameter since its a get request to get the actual data 
+        const { stockTicketer } = req.query; 
+        if (!stockTicketer || stockTicketer.trim() === ""){
+            return res.status(400).json({ 
+                Error: 'Stock ticketer is required'
+            }); 
+        } 
+
+        // now we can get the stock data here as such 
+        const stockData = await fetchStockData(stockTicketer); 
+        // now call the map function to organize the data  
+        const organizedStockData = stockData.data?.map((stock) => { 
+            dailyDifference: stock.close - stock.open
+            dailyChangePercent: ((stock.close - stock.open) / stock.open * 100).toFixed(2)
+            //volatility 
+            dailyRange: stock.high - stock.low
+            volatilityPercent: ((stock.high - stock.low) / stock.open * 100).toFixed(2), 
+            //volume strength 
+            volumeInMillions: (stock.volume / 1000000).toFixed(2) 
+            //Price p osition in daily range 
+            closedNearHigh: ((stock.close - stock.low) / (stock.high - stock.low) * 100).toFixed(0)
+        })
+    } catch (error) { 
+
+    }
+} 
+
+module.exports = organizeAnalysis; 
--- backend/routes/organizesend.route.js
+++ backend/routes/organizesend.route.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const organizeandsend = express.Router(); 
-const oasController = require('../controller/organizesend.controller.js'); 
+const  organizeAnalysis = require('../controller/organizesend.controller.js'); 
 
 // get requeuest becase we are getting the data 
-organizeandsend.get('/analysis', oasController); 
+organizeandsend.get('/analysis',  organizeAnalysis); 
 
 module.exports = organizeandsend;
