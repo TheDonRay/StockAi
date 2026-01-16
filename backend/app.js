4edcbfc6b49c15fdc991a32485007f468d3a34c9
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-16 12:47:50 -0500

    setting up the backend for this server

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,13 @@
 const express = require('express'); 
 const app = express(); 
 
+app.use(express.json());  
+
+//simple backend route to make sure backend is correctly running 
+app.get('/', (req, res) => { 
+    res.json({ 
+        Server: 'Successfully Running'
+    }); 
+}); 
+
+module.exports = app; 
