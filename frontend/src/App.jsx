ef2072a9a0b08ffb53a5ace742f303c0ac49442c
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-15 15:48:55 -0500

    added the analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Homepage from "./components/Homepage.jsx";
-import SignUpPage from "./components/Signuppage.jsx";  
-import DashPage from "./components/Dashboard.jsx"; 
+import SignUpPage from "./components/Signuppage.jsx";
+import DashPage from "./components/Dashboard.jsx";
 
 function App() {
   return (
     <BrowserRouter>
+      <Analytics />
       <Routes>
         {/* Example: <Route path="/" element={<Home />} /> */}
         <Route path="/" element={<Homepage />} /> 
